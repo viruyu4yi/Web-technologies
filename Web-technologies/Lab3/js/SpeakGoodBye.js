c0cf6860fa6cc92598a9191f9a1f2a5f0fe67385
viruyu4yi <73821183+viruyu4yi@users.noreply.github.com>
2023-12-28 16:34:05 +0200

    Delete Web-technologies directory

--- Web-technologies/Lab3/js/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-function speakBye() {
-    var speakWord = "Good Bye";
-
-    function speak(name) {
-        console.log(speakWord + " " + name);
-    }
-    return speak;
-}
--- Web-technologies/Lab3/js/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-function speakHello() {
-    var speakWord = "Hello";
-
-    function speak(name) {
-        console.log(speakWord + " " + name);
-    }
-    return speak;
-}
